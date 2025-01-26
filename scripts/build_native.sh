0c0713e5685bade28cf4f623659631cf8ff39975
dumble555 <>
2025-01-27 04:33:33 +0530

    2nd

--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# Build to assets instead of native/bin
+
+ARCH=${1:-x64}
+
 mkdir -p assets/native/linux
 
-# C
-gcc native/c/hello.c -o assets/native/linux/hello_c
+case $ARCH in
+  x64)
+    gcc native/c/hello.c -o assets/native/linux/hello_c
+    g++ native/cpp/hello.cpp -o assets/native/linux/hello_cpp
+    cd native/rust && cargo build --release && cp target/release/hello_rust ../../assets/native/linux/
+    cd ../go && GOOS=linux GOARCH=amd64 go build -o ../../assets/native/linux/hello_go hello.go
+    ;;
 
-# C++
-g++ native/cpp/hello.cpp -o assets/native/linux/hello_cpp
-
-# Rust
-cd native/rust && cargo build --release && cp target/release/hello_rust ../../assets/native/linux/
-
-# Go
-cd ../go && GOOS=linux GOARCH=amd64 go build -o ../../assets/native/linux/hello_go hello.go
-
-# Set permissions
-chmod +x ../../assets/native/linux/*
+  arm64)
+    # Native compilation on ARM runner
+    gcc native/c/hello.c -o assets/native/linux/hello_c
+    g++ native/cpp/hello.cpp -o assets/native/linux/hello_cpp
+    cd native/rust && cargo build --release && cp target/release/hello_rust ../../assets/native/linux/
+    cd ../go && GOOS=linux GOARCH=arm64 go build -o ../../assets/native/linux/hello_go hello.go
+    ;;
+esac
